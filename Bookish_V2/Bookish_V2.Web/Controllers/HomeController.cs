248d8d76582606634249b5c0274e6de259cb3ef0
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-07 16:42:55 +0100

    Moved the catalogue off the index page.

--- Bookish_V2/Bookish_V2.Web/Controllers/HomeController.cs
+++ Bookish_V2/Bookish_V2.Web/Controllers/HomeController.cs
@@ -14,21 +14,20 @@ namespace Bookish_V2.Web.Controllers
 	{
 		public ActionResult Index()
 		{
-			var x = new SqlConnection();
-			var db = new BookishConnection();
-			var catalogue = new LibraryCatalogueViewModel
-			{
-				inventory = db.GetInventory()
-			};
-
-			return View("Index", catalogue);
+			return View();
 		}
 
-		public ActionResult About()
+		public ActionResult Catalogue()
 		{
+			var bookishConnection = new BookishConnection();
+			var catalogue = new LibraryCatalogueViewModel
+			{
+				inventory = bookishConnection.GetInventory()
+			};
+
 			ViewBag.Message = "Your application description page.";
 
-			return View();
+			return View("Catalogue", catalogue);
 		}
 
 		public ActionResult Contact()
