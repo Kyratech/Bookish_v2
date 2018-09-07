8dfd2c6bfd6eed4de6b5fa29c6e3bbf09a119d2a
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-07 17:13:59 +0100

    Refactored out duplication in BookishConnection.

--- Bookish_V2/Bookish_V2.DataAccessFmwk/BookishConnection.cs
+++ Bookish_V2/Bookish_V2.DataAccessFmwk/BookishConnection.cs
@@ -11,32 +11,32 @@ namespace Bookish_V2.DataAccessFmwk
 	{
 		public List<Book> GetAllBooks()
 		{
-			IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+			IDbConnection db = GetBookishConnection();
 			string sqlString = "SELECT * FROM [Books]";
 			return (List<Book>)db.Query<Book>(sqlString);
 		}
 
 		public List<Item> GetAllItems()
 		{
-			var x = new SqlConnection();
-			IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 			string sqlString = "SELECT * FROM [Items] INNER JOIN [Books] ON Items.BookId=Books.BookId";
-			var items = db.Query<Item, Book, Item>(
-				sqlString,
-				(item, book) =>
-				{
-					item.BookDetails = book;
-					return item;
-				},
-				splitOn: "BookId").Distinct().ToList();
-			return items;
+			return GetItemsByQuery(sqlString);
 		}
 
 		public List<Item> GetAllItems(string searchTerm)
 		{
-			var x = new SqlConnection();
-			IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 			string sqlString = "SELECT * FROM [Items] INNER JOIN [Books] ON Items.BookId=Books.BookId WHERE [Title]='" + searchTerm + "' OR [Authors]='" + searchTerm + "'";
+			return GetItemsByQuery(sqlString);
+		}
+
+		private IDbConnection GetBookishConnection()
+		{
+			return new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+		}
+
+		private List<Item> GetItemsByQuery(string sqlString)
+		{
+			IDbConnection db = GetBookishConnection();
+
 			var items = db.Query<Item, Book, Item>(
 				sqlString,
 				(item, book) =>
@@ -51,29 +51,20 @@ namespace Bookish_V2.DataAccessFmwk
 		public SortedDictionary<Book, int> GetInventory()
 		{
 			var allItems = this.GetAllItems();
-			var inventory = new SortedDictionary<Book, int>();
-
-			foreach (var item in allItems)
-			{
-				if (inventory.ContainsKey(item.BookDetails))
-				{
-					inventory[item.BookDetails] = inventory[item.BookDetails] + 1;
-				}
-				else
-				{
-					inventory.Add(item.BookDetails, 1);
-				}
-			}
-
-			return inventory;
+			return GenerateInventoryFromListOfAllItems(allItems);
 		}
 
 		public SortedDictionary<Book, int> GetInventory(string searchTerm)
 		{
 			var allItems = this.GetAllItems(searchTerm);
+			return GenerateInventoryFromListOfAllItems(allItems);
+		}
+
+		private SortedDictionary<Book, int> GenerateInventoryFromListOfAllItems(List<Item> listOfAllItems)
+		{
 			var inventory = new SortedDictionary<Book, int>();
 
-			foreach (var item in allItems)
+			foreach (var item in listOfAllItems)
 			{
 				if (inventory.ContainsKey(item.BookDetails))
 				{
