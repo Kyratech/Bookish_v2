1364c3bfacaae730a2e7b2ed09f02f8d7cffd8f1
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-10 12:09:20 +0100

    Moved the library views into their own folder.

--- Bookish_V2/Bookish_V2.Web/Controllers/HomeController.cs
+++ Bookish_V2/Bookish_V2.Web/Controllers/HomeController.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Bookish_V2.DataAccessFmwk;
-using Bookish_V2.Web.Models;
-using System.Data.SqlClient;
-
+﻿using System.Web.Mvc;
 
 namespace Bookish_V2.Web.Controllers
 {
@@ -15,44 +7,6 @@ namespace Bookish_V2.Web.Controllers
 		public ActionResult Index()
 		{
 			return View();
-		}
-
-		public ActionResult Catalogue()
-		{
-			var bookishConnection = new BookishConnection();
-			var catalogue = new LibraryCatalogueViewModel
-			{
-				inventory = bookishConnection.GetInventory()
-			};
-
-			return View("Catalogue", catalogue);
-		}
-
-		[HttpPost]
-		public ActionResult Catalogue(LibraryCatalogueViewModel viewModel)
-		{
-			var bookishConnection = new BookishConnection();
-			var catalogue = new LibraryCatalogueViewModel
-			{
-				inventory = bookishConnection.GetInventory(viewModel.SearchTerm)
-			};
-
-			return View("Catalogue", catalogue);
-		}
-
-		public ActionResult Book(int bookId)
-		{
-			var bookishConnection = new BookishConnection();
-			var bookDetails = bookishConnection.GetBookDetails(bookId);
-			var bookDetailsViewModel = new LibraryItemViewModel
-			{
-				AvailableCopies = bookDetails.AvailableCopies,
-				TotalCopies = bookDetails.TotalCopies,
-				Book = bookDetails.BookDetails,
-				BorrowedItemDetails = bookDetails.BorrowedItemDetails
-			};
-
-			return View(bookDetailsViewModel);
 		}
 	}
 }
---
+++ Bookish_V2/Bookish_V2.Web/Controllers/LibraryController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using Bookish_V2.DataAccessFmwk;
+using Bookish_V2.Web.Models;
+
+namespace Bookish_V2.Web.Controllers
+{
+    public class LibraryController : Controller
+    {
+		public ActionResult Catalogue()
+	    {
+		    var bookishConnection = new BookishConnection();
+		    var catalogue = new LibraryCatalogueViewModel
+		    {
+			    inventory = bookishConnection.GetInventory()
+		    };
+
+		    return View("Catalogue", catalogue);
+	    }
+
+	    [HttpPost]
+	    public ActionResult Catalogue(LibraryCatalogueViewModel viewModel)
+	    {
+		    var bookishConnection = new BookishConnection();
+		    var catalogue = new LibraryCatalogueViewModel
+		    {
+			    inventory = bookishConnection.GetInventory(viewModel.SearchTerm)
+		    };
+
+		    return View("Catalogue", catalogue);
+	    }
+
+	    public ActionResult Book(int bookId)
+	    {
+		    var bookishConnection = new BookishConnection();
+		    var bookDetails = bookishConnection.GetBookDetails(bookId);
+		    var bookDetailsViewModel = new LibraryItemViewModel
+		    {
+			    AvailableCopies = bookDetails.AvailableCopies,
+			    TotalCopies = bookDetails.TotalCopies,
+			    Book = bookDetails.BookDetails,
+			    BorrowedItemDetails = bookDetails.BorrowedItemDetails
+		    };
+
+		    return View(bookDetailsViewModel);
+	    }
+	}
+}
